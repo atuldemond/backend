82d6a8a3ff0e90a488ea75c11764d9d061bda87a
atuldemond <atuldemond@gmail.com>
2024-06-01 15:58:39 +0530

    learning on ejs templating complete

--- express_dir/ejs_templating/app.js
+++ express_dir/ejs_templating/app.js
@@ -5,13 +5,59 @@ const cors = require("cors");
 const port = 3000;
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/error", (req, res, next) => {
+  try {
+    res.send("hey");
+  } catch (error) {
+    next(error);
+  }
+});
 
-app.get("*/", (req, res) => {
+app.get("/check ", (req, res) => {
+  console.log(req.query.name);
+  console.log(req.query.password);
+
+  res.send("form Submited");
+});
+
+app.post("/check", (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.password);
+
+  res.send("form Submited");
+});
+app.patch("/check", (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.password);
+
+  res.send("form Submited");
+});
+app.put("/check", (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.password);
+
+  res.send("form Submited");
+});
+app.delete("/check", (req, res) => {
+  console.log(req.body.name);
+  console.log(req.body.password);
+
+  res.send("form Submited");
+});
+
+app.get("*", (req, res) => {
   res.send("Page NOt Found");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send("inetrnal server error" + err.message);
 });
 
 app.listen(port);
