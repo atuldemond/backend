740df3a2b414fb8131c6652daa0c00606c2c60ca
atuldemond <atuldemond@gmail.com>
2024-07-12 12:26:57 +0530

    login with cookie

--- practise/2.Encryption/app.js
+++ practise/2.Encryption/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const db = require("./configs/mongoose");
 const userModel = require("./models/useModel");
@@ -12,20 +14,31 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).send("An error occurred while rendering the page");
+  }
 });
+
+//create register router with bcrypt
 app.post("/register", async (req, res) => {
-  const { name, age, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await userModel.create({
-    name,
-    age,
-    email,
-    password: hashedPassword,
-  });
-  res.send("Registration successful");
+  try {
+    const { name, age, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.create({
+      name,
+      age,
+      email,
+      password: hashedPassword,
+    });
+    res.send("Registration successful");
+  } catch (error) {
+    res.status(500).send("An error occurred during registration");
+  }
 });
 
 app.post("/login", async (req, res) => {
@@ -37,6 +50,10 @@ app.post("/login", async (req, res) => {
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (isPasswordValid) {
+      const token = jwt.sign({ userId: user._id }, "secret", {
+        expiresIn: "60s",
+      });
+      res.cookie("token", token, { maxAge: 600000, httpOnly: true });
       res.send("Login successful");
     } else {
       res.send("Login failed: Incorrect password");
@@ -46,10 +63,39 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.use((req, res) => {
-  res.status(404).send("You are on the wrong path");
+//send page with jwt token and checking with data save inside cookie user_id
+app.post("/seepage", async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).send("No token provided");
+    }
+
+    const decoded = jwt.verify(token, "secret");
+    const user = await userModel.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("An error occurred while verifying the token");
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use((req, res) => {
+  try {
+    res.status(404).send("You are on the wrong path");
+  } catch (error) {
+    res.status(500).send("An error occurred while handling the request");
+  }
 });
+
+try {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+} catch (error) {
+  console.error("An error occurred while starting the server:", error);
+}
