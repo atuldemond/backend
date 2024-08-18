eaa9263cf80930c978e6adbb69d2ab00f9222d38
atuldemond <atuldemond@gmail.com>
2024-08-18 15:14:43 +0530

    formalities

--- khatabook_dummy/app.js
+++ khatabook_dummy/app.js
@@ -1,34 +1,38 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-//checking routes
-//routes
-const authRoutes = require("./routes/authRoutes");
-const homeRoutes = require("./routes/homeRoutes");
-const profileRoutes = require("./routes/profileRoutes");
+try {
+  const express = require("express");
+  const app = express();
+  const path = require("path");
+  //checking routes
+  //routes
+  const authRoutes = require("./routes/authRoutes");
+  const homeRoutes = require("./routes/homeRoutes");
+  const profileRoutes = require("./routes/profileRoutes");
 
-//ejs configuration
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+  //ejs configuration
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.set("view engine", "ejs");
+  app.use(express.static(path.join(__dirname, "public")));
 
-//database//
-const connectionDb = require("./configs/mongoose");
-connectionDb();
-const userModel = require("./models/user-model");
-const postModel = require("./models/post-model");
+  //database//
+  const connectionDb = require("./configs/mongoose");
+  connectionDb();
+  const userModel = require("./models/user-model");
+  const postModel = require("./models/post-model");
 
-//routes
+  //routes
 
-app.use("/auth", authRoutes);
-app.use("/home", homeRoutes);
-app.use("/profile", profileRoutes);
+  app.use("/auth", authRoutes);
+  app.use("/home", homeRoutes);
+  app.use("/profile", profileRoutes);
 
-app.use("*", (req, res) => {
-  res.render("pagenotfound");
-});
-//server is running
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is Running");
-});
+  app.use("*", (req, res) => {
+    res.render("pagenotfound");
+  });
+  //server is running
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is Running");
+  });
+} catch (error) {
+  console.error("An error occurred: ", error);
+}
